a8a63209781940aeb3292a51665fc5b38468f0e8
AlexVesely <veselyalex11@gmail.com>
2024-12-21 10:03:11 +0000

    Write down what to include in this class

--- SearchingAlgorithms.java
+++ SearchingAlgorithms.java
@@ -1,2 +1,12 @@
 public class SearchingAlgorithms {
+  // Linear Search, Binary Search, Jump Search, Exponential Search,
+  // Built in , Arrays.binarySearch() , Collections.binarySearch(), List.contains(), Set.contains()
+
+
+
+
+
+
+
+  
 }

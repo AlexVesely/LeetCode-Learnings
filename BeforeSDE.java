813083f545d5c82435df7018e24aff01a089542b
AlexVesely <veselyalex11@gmail.com>
2024-12-22 19:13:04 +0100

    Create BeforeSDE.java

---
+++ BeforeSDE.java
@@ -0,0 +1,6 @@
+public class BeforeSDE {
+    // Sliding Window https://leetcode.com/problems/minimum-size-subarray-sum/description/
+    // Find if a pair in a sorted array are equal to a specific value https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+    // HashSet, does a String have duplicates https://leetcode.com/problems/contains-duplicate/description/
+    // Create Binary Search https://leetcode.com/problems/binary-search/description/
+}

be20041b1b2141d472c87f4ad8e90a73f024f4e4
AlexVesely <veselyalex11@gmail.com>
2024-12-26 15:39:25 +0100

    Create Adjacency List

---
+++ DataStructures/Graph/AdjacencyList.java
@@ -0,0 +1,47 @@
+package DataStructures.Graph;
+
+import java.util.ArrayList;
+
+public class AdjacencyList {
+    private final ArrayList<ArrayList<Integer>> adjList;
+    private final boolean isDirected;
+
+    public AdjacencyList(int numVertices, boolean isDirected) {
+        this.isDirected = isDirected;
+        adjList = new ArrayList<>();
+        for (int i = 0; i < numVertices; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        adjList.get(src).add(dest);
+        if (!isDirected) {
+            adjList.get(dest).add(src); // Undirected graph has src <-> dest
+        }
+    }
+
+    public void printList() {
+        for (int i = 0; i < adjList.size(); i++) {
+            System.out.print(i + " -> ");
+            for (int neighbor : adjList.get(i)) {
+                System.out.print(neighbor);
+                if (neighbor < adjList.get(i).size()) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        AdjacencyList list = new AdjacencyList(4, true);
+        list.addEdge(2, 3);
+        list.addEdge(0, 1);
+        list.addEdge(0, 3);
+        list.addEdge(1, 2);
+        list.printList();
+    }
+}
--- DataStructures/Graph/AdjacencyMatrix.java
+++ DataStructures/Graph/AdjacencyMatrix.java
@@ -1,9 +1,9 @@
 package DataStructures.Graph;
 
 public class AdjacencyMatrix {
-    private int[][] matrix;
-    private int numVertices;
-    private boolean isDirected;
+    private final int[][] matrix;
+    private final int numVertices;
+    private final boolean isDirected;
     // We could also add a 'weight' attribute between the vertices if we wanted
 
     public AdjacencyMatrix(int numVertices, boolean isDirected) {
@@ -15,7 +15,7 @@ public class AdjacencyMatrix {
     // REMEMBER THE FIRST INDEX IS 0 NOT 1
     public void addEdge(int src, int dest) {
         matrix[src][dest] = 1;
-        if (!isDirected) { // In undirected graphs show the src <-> dest
+        if (!isDirected) { // In undirected graphs show src <-> dest
             matrix[dest][src] = 1;
         }
     }
@@ -30,11 +30,11 @@ public class AdjacencyMatrix {
     }
 
     public static void main(String[] args) {
-        AdjacencyMatrix matrix = new AdjacencyMatrix(4,true);
-        matrix.addEdge(0,1);
-        matrix.addEdge(1,2);
-        matrix.addEdge(1,3);
-        matrix.addEdge(3,2);
+        AdjacencyMatrix matrix = new AdjacencyMatrix(4, true);
+        matrix.addEdge(0, 1);
+        matrix.addEdge(1, 2);
+        matrix.addEdge(1, 3);
+        matrix.addEdge(3, 2);
         matrix.printMatrix();
     }
 }

067db8914c77a54d9bfedfbf4a25366cd7fb9497
AlexVesely <veselyalex11@gmail.com>
2024-12-26 15:17:03 +0100

    Create Adjacency Matrix

--- DataStructures/Graph/AdjacencyMatrix.java
+++ DataStructures/Graph/AdjacencyMatrix.java
@@ -3,20 +3,23 @@ package DataStructures.Graph;
 public class AdjacencyMatrix {
     private int[][] matrix;
     private int numVertices;
+    private boolean isDirected;
+    // We could also add a 'weight' attribute between the vertices if we wanted
 
-    // Constructor
-    public AdjacencyMatrix(int numVertices) {
+    public AdjacencyMatrix(int numVertices, boolean isDirected) {
         this.numVertices = numVertices;
         this.matrix = new int[numVertices][numVertices];
+        this.isDirected = isDirected;
     }
 
-    // Add Edge
-    public void addEdge(int src, int dest, int weight) {
-        matrix[src][dest] = weight;
-        matrix[dest][src] = weight; // For undirected graph
+    // REMEMBER THE FIRST INDEX IS 0 NOT 1
+    public void addEdge(int src, int dest) {
+        matrix[src][dest] = 1;
+        if (!isDirected) { // In undirected graphs show the src <-> dest
+            matrix[dest][src] = 1;
+        }
     }
 
-    // Print the matrix
     public void printMatrix() {
         for (int i = 0; i < numVertices; i++) {
             for (int j = 0; j < numVertices; j++) {
@@ -27,7 +30,12 @@ public class AdjacencyMatrix {
     }
 
     public static void main(String[] args) {
-
+        AdjacencyMatrix matrix = new AdjacencyMatrix(4,true);
+        matrix.addEdge(0,1);
+        matrix.addEdge(1,2);
+        matrix.addEdge(1,3);
+        matrix.addEdge(3,2);
+        matrix.printMatrix();
     }
 }
 

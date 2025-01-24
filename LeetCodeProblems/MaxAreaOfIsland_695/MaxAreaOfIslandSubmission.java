573f4b3904ce5929d932b3954ab8cf0b926aa52b
AlexVesely <veselyalex11@gmail.com>
2025-01-24 19:34:19 +0000

    Create Max Area Island Class

---
+++ LeetCodeProblems/MaxAreaOfIsland_695/MaxAreaOfIslandSubmission.java
@@ -0,0 +1,15 @@
+package LeetCodeProblems.MaxAreaOfIsland_695;
+
+public class MaxAreaOfIslandSubmission {
+
+
+    public int maxAreaOfIsland(int[][] grid) {
+
+
+        return 3;
+    }
+
+
+
+
+}
